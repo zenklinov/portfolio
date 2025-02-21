d2e0ec702da24bc41ee9d3f0d750e1b13623f073
Amanatullah Pandu Zenklinov <91674858+zenklinov@users.noreply.github.com>
2025-02-21 21:04:48 +0700

    Update projects.js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -11,7 +11,7 @@ let render_projects = (slug) => {
 
     let projects_obj = [
         {
-            image: 'assets/images/mentors.jpg',
+            image: 'https://ollama.com/public/ollama.png',
             link: 'https://github.com/zenklinov/Streamlit-Ollama-Llama3.2-Chat',
             title: 'TEST',
             demo: false,
